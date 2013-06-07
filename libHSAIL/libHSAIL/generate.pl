f85108898dec5fadec4fd11a5c68fefed67e45b6
Huy Bui <huy@multicorewareinc.com>
2013-06-07 13:39:51 -0500

    Fix generating scripts to prepend AMD's license to generated files

--- libHSAIL/libHSAIL/generate.pl
+++ libHSAIL/libHSAIL/generate.pl
@@ -191,6 +191,46 @@ while(pos($_) < length($_)) {
 my $textIncludeItems = "#include \"HSAILItems.h\"\n";
 my $textBeginNamespace = "namespace HSAIL_ASM {\n\n";
 my $textEndNamespace = "\n} // namespace HSAIL_ASM\n";
+my $textLicense = "// University of Illinois/NCSA
+// Open Source License
+// 
+// Copyright (c) 2013, Advanced Micro Devices, Inc.
+// All rights reserved.
+// 
+// Developed by:
+// 
+//     HSA Team
+// 
+//     Advanced Micro Devices, Inc
+// 
+//     www.amd.com
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the \"Software\"), to deal with
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+// of the Software, and to permit persons to whom the Software is furnished to do
+// so, subject to the following conditions:
+// 
+//     * Redistributions of source code must retain the above copyright notice,
+//       this list of conditions and the following disclaimers.
+// 
+//     * Redistributions in binary form must reproduce the above copyright notice,
+//       this list of conditions and the following disclaimers in the
+//       documentation and/or other materials provided with the distribution.
+// 
+//     * Neither the names of the LLVM Team, University of Illinois at
+//       Urbana-Champaign, nor the names of its contributors may be used to
+//       endorse or promote products derived from this Software without specific
+//       prior written permission.
+// 
+// THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+// CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS WITH THE
+// SOFTWARE.\n";
 
 #### POSTPROCESSING ####
 
@@ -310,6 +350,7 @@ sub findDefaultFieldValue {
 }
 
 sub makeHSAILInitBrig {
+        printf $textLicense;
 	for my $s (@sortedStructs) {
 		next if $s->{generic};
         	my $w = $s->{wrapper};
@@ -426,6 +467,7 @@ sub printComments {
 }
 
 sub makeWrappers {
+        printf $textLicense;
 	print map { "class $_->{wname};\n" } grep { !$_->{nowrap} } @sortedStructs;
 	print "\n\n";
 
@@ -554,6 +596,7 @@ sub makeWrappers {
 };
 
 sub makeDispatchByItem {
+        printf $textLicense;
 	for my $s (@sortedStructs) {
 		next if $s->{nowrap};
 		next unless $s->{generic};
@@ -571,6 +614,7 @@ sub makeDispatchByItem {
 }
 
 sub makeEnumFlds {
+        printf $textLicense;
 	my $pfx = "";
 	for my $s (@sortedStructs) {
 		next if $s->{nowrap} or $s->{generic};
@@ -600,6 +644,7 @@ sub makeEnumFlds {
 }
 
 sub makeHSAILBrigInitializers {
+        printf $textLicense;
 	for my $s (@sortedStructs) {
 		next if $s->{nowrap};
 		next if $s->{generic};
@@ -620,6 +665,7 @@ sub makeMyCode {
 };
 
 sub makePrint {
+        printf $textLicense;
 	print @_;
 }
 
